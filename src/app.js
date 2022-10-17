9b4ff5a3b851c49376d68f38108f271b18c29d17
melaniem21 <melaniealexandra.m@gmail.com>
2022-10-17 02:52:18 +0000

    generador subido

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generadordeexcusa();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generadordeexcusa = () => {
+  var aleatorio = Math.random();
+  var who = ["mi abuela", "el perro", "mi pajaro", "un loco"];
+  var verb = ["se comio", "se robo", "rompio", "escondió"];
+  var what = ["mi tarea!!!", "mi ropa!!!", "mi zapato!!!", "las llaves!!!"];
+  var when = [
+    "mientras rezaba",
+    "antes de la clase",
+    "en la calle",
+    "cuando almorzaba"
+  ];
+
+  var whoindex = Math.floor(Math.random() * who.length);
+  var verbindex = Math.floor(Math.random() * verb.length);
+  var whatindex = Math.floor(Math.random() * what.length);
+  var whenindex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoindex] +
+    " " +
+    verb[verbindex] +
+    " " +
+    what[whatindex] +
+    " " +
+    when[whenindex]
+  );
 };
